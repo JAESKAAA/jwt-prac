f6c0e2bff26daaada7e2b68565ee40ff770dfbf6
jaeskaaa <jaeskaaa@gmail.com>
2022-02-05 17:13:59 +0900

    환경설정 및 user 엔티티생성

---
+++ src/main/java/com/prac/jwtserver/entity/User.java
@@ -0,0 +1,28 @@
+package com.prac.jwtserver.entity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String roles; //USER, ADMIN
+
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+}
